b1f896f5620c8c84978354144e456c36ca99a5e0
jeremyschade@hotmail.com <jeremyschade@hotmail.com>
2023-10-01 16:48:11 +0200

    GameMode + Controller + Pawn for the tactical Base (combat)

--- TacticalBase/Source/TacticalBase/GameMode_TB.cpp
+++
@@ -1,9 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "GameMode_TB.h"
-
-AGameMode_TB::AGameMode_TB() 
-{
-	DefaultPawnClass = APawnTB::StaticClass();
-}
